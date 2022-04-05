2449925727c23794394ba49cbe3b98616a2e24d7
alexa <alexandreordas@yahoo.com>
2022-04-05 11:48:59 +0200

    Initialisation du projet

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import SignUpForm from './SignUpForm'
+
+class App extends Component {
+    render() {
+        return <SignUpForm/>
+    }
+}
+
+export default App
