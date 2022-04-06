2d20f6d0c3a36a83ee6929b8e96bed4831f42417
alexa <alexandreordas@yahoo.com>
2022-04-06 16:44:32 +0200

    Ajout de la fonction Edit & Update des expenses

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ class App extends Component {
         expenses: [],
         nextExpenseId: 0,
         isCreatingExpense: false,
+        currentlyEditedExpense: null
     }
 
     showCreationForm = () => {
@@ -24,7 +25,36 @@ class App extends Component {
         })
     }
 
+    showEditionForm = expense => {
+        this.setState({currentlyEditedExpense: expense})
+    }
+    updateExpense = expenseInfos => {
+        const { expenses } = this.state
+        const expenseIndex = expenses.findIndex(e => e.id === expenseInfos.id )
+        const expensesBefore = expenses.slice(0, expenseIndex)
+        const expensesAfter = expenses.slice(expenseIndex + 1)
+        this.setState({
+            expenses: [
+                ...expensesBefore,
+                expenseInfos,
+                ...expensesAfter
+            ],
+            currentlyEditedExpense: null
+        })
+    }
+
     render() {
+        if (this.state.currentlyEditedExpense) {
+            return(
+                <Fragment>
+                    <h2>Edit expense</h2>
+                    <ExpenseForm
+                        expense={this.state.currentlyEditedExpense}
+                        onSubmit={this.updateExpense}
+                    />
+                </Fragment>
+            )
+        }
         if (this.state.isCreatingExpense) {
             return (
                 <Fragment>
@@ -41,6 +71,7 @@ class App extends Component {
                     <ul>
                         {this.state.expenses.map(expense => (
                             <li key={expense.id}>
+                                <button onClick={ () => this.showEditionForm(expense)}>Edit</button>
                                 {expense.title} : {expense.amount} € spent on{' '}
                                 {new Date(expense.date).toDateString()}
                             </li>
